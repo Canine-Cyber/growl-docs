8defb2d4671351ca72a7489a5aa438296c8704dd
N-G-Asker <n.g.asker@gmail.com>
2024-12-02 12:35:20 -0500

    Dealt with bug relating to locating image assets correctly in production.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
+  assetPrefix: isProd ? "/your-repository-name/" : "",
+  basePath: isProd ? "/your-repository-name" : "",
   output: "export", // <=== enables static exports
 };
 
